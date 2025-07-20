d1fa611ea03176d4f59d5fb6da58eac95a6765e8
JUSTIN47 <86733002+justinrivera47@users.noreply.github.com>
2025-07-20 16:25:46 -0400

    update
    pushing changes

--- api/ping.ts
+++ api/ping.ts
@@ -6,9 +6,9 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-// CORS (temp dev version)
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Replace with localhost:5173 later
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
